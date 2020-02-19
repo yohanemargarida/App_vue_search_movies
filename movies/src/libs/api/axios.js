d15f917ae5e9aba7e9c216a5f89de542d7af238d
Yohane Braga <yohane85@gmail.com>
2020-02-19 18:17:09 -0300

    connect with correct endpoint

--- movies/src/libs/api/axios.js
+++ movies/src/libs/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import Vue from 'vue'
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/search/movie?api_key=69f81ecbaec99746aae10a311d0878e8&'
-
 Vue.use({
     install(Vue) {
-        Vue.prototype.$http = axios
+        Vue.prototype.$http = axios.create({
+            baseURL: 'https://api.themoviedb.org/3/'
+        })
     }
 })
--- movies/src/views/Details.vue
+++ movies/src/views/Details.vue
@@ -1,65 +1,43 @@
 <template>
   <div v-if="movie">
     <h1>{{ movie.title }}</h1>
-    <p>{{ movie.overview}}</p>
-    <p>{{ genres }}</p>
+    <p>{{ movie.overview }}</p>
+    
 
-    <p v-for="genre in genres" :key="genre.id"></p>
+    
     <img :src="'https://image.tmdb.org/t/p/w500/' + movie.backdrop_path" />
   </div>
 </template>
 
 <script>
-import axios from "axios";
 export default {
   data() {
     return {
-      genres: "",
-      movieId: "",
+      movie: "",
       list: ""
     };
   },
   computed: {
     listMovies() {
       return this.$store.state.listMovies;
-    },
-    movie: {
-      get() {
-        let idFiltered = this.$store.state.listMovies.find(movie => {
-          return movie.id == this.$route.query.id;
-        });
-        return idFiltered;
-      },
-      set(idFiltered) {
-        return (this.movieId = idFiltered);
-      }
     }
   },
   methods: {
-    getResult() {
-      axios
+    getMovie(id) {
+      this.$http
         .get(
-          "https://api.themoviedb.org/3/search/movie?api_key=69f81ecbaec99746aae10a311d0878e8&id=" +
-            this.movieId
+          "movie/" + id + "?api_key=69f81ecbaec99746aae10a311d0878e8" 
         )
-        .then(response => {
-          this.list = response.data.results;
+        .then(response => { console.log(response.data);
+          this.movie = response.data;
         })
         .catch(error => console.log(error));
-      console.log(this.listMovies);
-    },
-    getGenres() {
-      axios
-        .get(
-          "https://api.themoviedb.org/3/movie?api_key=69f81ecbaec99746aae10a311d0878e8"
-        )
-        .then(response => {
-          this.genres = response.data.results;
-          console.log(this.genres);
-        })
-        .catch(error => console.log(error));
-      console.log(this.genres);
     }
+  },
+  mounted() {console.log('to aqui no mounted');
+    
+      this.getMovie(this.$route.query.id)
+   
   }
 };
 </script>
